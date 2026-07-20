f23ad269fbb4604ebbb7e3dfdeb95f2e6659f8d1
Matt Bovel <matthieu@bovel.net>
2026-07-21 01:04:28 +0200

    Increase warmup iterations

--- evaluation/schmid/src/main/scala/bench/CompilationBenchmarks.scala
+++ evaluation/schmid/src/main/scala/bench/CompilationBenchmarks.scala
@@ -18,7 +18,7 @@ import org.openjdk.jmh.annotations.{
   value = 1,
   jvmArgsPrepend = Array("-XX:+PrintCommandLineFlags", "-Xms8G", "-Xmx8G")
 )
-@Warmup(iterations = 150)
+@Warmup(iterations = 180)
 @Measurement(iterations = 20)
 @BenchmarkMode(Array(org.openjdk.jmh.annotations.Mode.SingleShotTime))
 @State(Scope.Benchmark)
